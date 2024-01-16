3c4c60c4f058e1e12058bccef1fd2637a258ad77
AlessioPirrotta <alessiopirrotta92@gmail.com>
2024-01-16 20:53:57 +0100

    bonus exersice completed

--- main.js
+++ main.js
@@ -6,24 +6,49 @@
 // azzurro ed emetto un messaggio in console con il numero della 
 // cella cliccata.
 
-const griglia= document.getElementById("grid")
-const Button= document.getElementById("btn")
+const griglia = document.getElementById("grid");
+const button = document.getElementById("btn");
+const difficoltà = document.getElementById("sct");
+const difficoltaIniziale = difficoltà.value;
 
 
-for(let i=0; i<100; i++){
-    let box = document.createElement("div")
-    box.classList.add ("box")
-    griglia.append(box)
-    box.innerHTML= [i+1]
-    box.addEventListener("click",function(){
-        this.classList.toggle("active")
-        console.log(this)
-    })
+button.addEventListener("click", function () {
+    griglia.classList.remove("d-none");
+
+    const difficoltaSelezionata = difficoltà.value;
+
+    creaGriglia(difficoltaSelezionata);
+});
+
+function creaGriglia(difficolta) {
+    
+    griglia.innerHTML = "";
+
+    if (difficolta === "easy") {
+        for (let i = 0; i < 100; i++) {
+            let box = document.createElement("div");
+            box.classList.add("box");
+            griglia.append(box);
+            box.innerHTML = i + 1;
+            box.addEventListener("click", function () {
+                this.classList.toggle("active");
+                console.log(this);
+            });
+        }
+    } else if (difficolta === "hard") {
+        for (let i = 0; i < 81; i++) {
+            let box = document.createElement("div");
+            box.classList.add("box2");
+            griglia.append(box);
+            box.innerHTML = i + 1;
+            box.addEventListener("click", function () {
+                this.classList.toggle("active");
+                console.log(this);
+            });
+        }
+    }
 }
 
-Button.addEventListener("click",function(){
-    griglia.classList.remove("d-none")
-})
 
 
 
